fec018c535cb1a42ca59576d89944016d7f08b00
augdog97 <augdog@me.com>
2020-11-30 01:08:41 -0500

    Added Base form functionality with click event and template vairables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+/* 
+  1. Creating the addArticle function that takes two parameters title and link which have types of input elements. 
+    - title and link are OBJECTS with type and are NOT the input values directly.
+    - The function is connected to the click event on the button on the form in app.comp.html
+    - We use string interpolation in the console.log statement. 
+    - Need to add parameters to the input elements so function can use them.
+  
+*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-angular';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
