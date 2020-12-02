42446653c0abffd57e69eebf3aef861365eabd23
augdog97 <augdog@me.com>
2020-12-02 01:50:17 -0500

    Finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,13 @@ import {Article} from './article/article.model';
     - title and link are OBJECTS with type and are NOT the input values directly.
     - The function is connected to the click event on the button on the form in app.comp.html
     - We use string interpolation in the console.log statement. 
+    - A new Article instance is created with the submitted title and URL (link)
+    - add it to the Article array 
+    - Clear the input fields. title and link are HTMLInputElement objects, this means we can set their properties. When the value of the property changes, the input tag on our page changes. 
     - Need to add parameters to the input elements so function can use them.
   2. Articles[] -> articles is an Array of Articles. The collection (the array) is typed. the Array is a collection that will only hold objects of type Article. 
     - We populate the array in the constructor using this.articles, new Article 
+  3. We are sorting the articles Array so the articles sort based on their score. 
 */
 
 @Component({
@@ -20,7 +24,14 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
   constructor() {
     this.articles = [
